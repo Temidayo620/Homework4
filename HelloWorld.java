9fc3bbfa9f867217a0585e5b4633c8c86f47dc62
Temidayo <tekundayo@njcu.edu>
2020-05-29 00:58:29 +0000

    My favorite food is Rice

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("I am taking CS407 because I want to program");
+		System.out.println("My favorite food is Rice");
 	}
 }
