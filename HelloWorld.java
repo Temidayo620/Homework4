ef1eb179adbdda997fa2738c31302a001304f692
Temidayo <tekundayo@njcu.edu>
2020-05-29 00:55:59 +0000

    I said the reason I am taking 407 was to be a programmer

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("My Favorite Programming Language is Java");
+		System.out.println("I am taking CS407 because I want to program");
 	}
 }
